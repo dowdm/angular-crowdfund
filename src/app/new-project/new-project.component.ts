23f0d6e42793d9d0a3b85c31daf022adbdbb8c45
Garnett Puett and Matt Dowdney <garnett@>
2018-05-09 11:12:46 -0700

    add new listing functionality

--- src/app/new-project/new-project.component.ts
+++ src/app/new-project/new-project.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Project } from '../project.model';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'app-new-project',
   templateUrl: './new-project.component.html',
-  styleUrls: ['./new-project.component.css']
+  styleUrls: ['./new-project.component.css'],
+  providers: [ProjectService]
 })
-export class NewProjectComponent implements OnInit {
+export class NewProjectComponent {
 
-  constructor() { }
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit() {
   }
 
+  submitForm(type: string, title: string,  description: string, starter: string,   goal: number) {
+  var newProject: Project = new Project(type, title, description, starter,   goal);
+  this.projectService.addProject(newProject);
+  }
 }
